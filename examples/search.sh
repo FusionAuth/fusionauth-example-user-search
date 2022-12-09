9987340d96200aea3247e1942731336b801b8ba4
Dan Moore <dan@fusionauth.io>
2022-12-08 20:10:05 -0700

    Built out complicated search, updated user data for it

--- examples/search.sh
+++ examples/search.sh
@@ -1,8 +1,10 @@
+#!/bin/sh
+
 FILE=$1
 
 API_KEY=90d8fb62-6f13-47d4-8ef6-1c3e687883c6
-#BASE_URL='http://localhost:9011'
-BASE_URL='https://sandbox.fusionauth.io'
+BASE_URL='http://localhost:9011'
+#BASE_URL='https://sandbox.fusionauth.io'
 
 curl -XPOST \
      -H 'Content-type: application/json' \
